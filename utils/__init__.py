4665c2445e077c45710587cd1b9fa79ea2cd9a44
thang09 <lythang09092004@gmail.com>
2025-06-07 23:28:18 +0700

    chore: delete unuse file

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,11 +5,8 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from utils.extract_tables_llm import get_is_has_header, get_is_new_section_context
 
 __all__ = [
     "Enrich_VertexAI",
     "Enrich_Openrouter", 
-    "get_is_has_header",
-    "get_is_new_section_context"
 ]
